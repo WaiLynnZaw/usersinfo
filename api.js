ee2b6f822d208575b26919d451a042a4e0067768
WaiLynnZaw <kolynn1500@gmail.com>
2016-03-03 13:31:15 +0630

    added update and delete operations

--- api.js
+++ api.js
@@ -2,8 +2,8 @@ var express = require("express");
 var mongojs = require("mongojs");
 var router = express.Router();
 
-// var db = mongojs('mydb',['users']);
-var db = mongojs('mongodb://wlz:root@ds019038.mlab.com:19038/myusersdb',['users']);
+var db = mongojs('mydb',['users']);
+// var db = mongojs('mongodb://wlz:root@ds019038.mlab.com:19038/myusersdb',['users']);
 
 router.get("/users", function(req,res) {
 	db.users.find(function(err,users){
@@ -22,6 +22,23 @@ router.get("/users/:id", function(req,res){
 	db.users.find({ _id: mongojs.ObjectId(id)}, function(err,user){
 		if(user) res.status(200).json(user);
 		else res.sendStatus(404);
+	});
+});
+
+router.put("/users/:id", function(req, res) {
+	var id = db.ObjectId(req.params.id);
+	var data = req.body;
+	data._id = id;
+
+	db.users.save(data, function(err, result){
+		res.status(200).json({"message":"Successfully Updated"});
+	});
+});
+
+router.delete("/users/:id", function(req, res) {
+	var id = db.ObjectId(req.params.id);
+	db.users.remove({ _id: id}, function(err, result) {
+		res.status(200).json({"message":"Successfully Deleted"});
 	});
 });
 
